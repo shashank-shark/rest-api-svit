55d140c75dab6462fa9a3515f6bc5f9b5e63a115
Shashank J <shashankreddy231@gmail.com>
2018-03-18 18:16:32 +0530

    action contorller for CURD

--- src/controller/college.js
+++ src/controller/college.js
@@ -5,7 +5,10 @@ import College from '../model/college';
 export default({ config, db }) => {
   let api = Router();
 
-  // '/v1/college/add'
+  //CRUD - Create Read Update Delete
+
+
+  // '/v1/college/add' - Create
   api.post('/add', (req, res) => {
     let newTeacher = new College();
     newTeacher.name = req.body.name;
@@ -17,5 +20,42 @@ export default({ config, db }) => {
       res.json({ messge: 'Teacher database saved successfully'});
     });
   });
+
+  //'v1'/college' - Read
+  api.get('/',(req, res) => {
+    College.find({}, (err, college) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(college);
+    });
+  });
+
+  //'v1/college/:id' - read particular id
+  api.get('/:id', (req, res) => {
+    College.findById(req.params.id, (err, teacher) => {
+      if (err) {
+        res.send(err);
+      }
+      res.json(teacher);
+    });
+  });
+
+  // 'v1/college/:id' - Update
+  api.put('/:id', (req, res) => {
+    College.findById(req.params.id, (err, teacher) => {
+      if(err) {
+        res.send(err);
+      }
+      teacher.name = req.body.name;
+      teacher.save(err => {
+        if(err) {
+          res.send(err);
+        }
+        res.json({message: "Teachers INFO Updated"});
+      });
+    });
+  });
+
   return api;
 }

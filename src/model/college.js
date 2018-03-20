bbb799ca19777af158a7a5b407c196aef378410f
Shashank J <shashankreddy231@gmail.com>
2018-03-20 18:52:31 +0530

    Added a feature of teacherType and review

--- src/model/college.js
+++ src/model/college.js
@@ -1,8 +1,21 @@
 import mongoose from 'mongoose';
+import Review from './review';
 let Schema = mongoose.Schema;
 
-let teacherSchema = new Schema({
-  name: String
+let collegeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  teacherType: {
+    type: String,
+    required: true
+  },
+  avgrating: String,
+  bestSubject: {
+    type: { type: String, default:'Subject' },
+  },
+  reviews: [{type: Schema.Types.ObjectId, ref:'Review'}]
 });
 
-module.exports = mongoose.model('Teacher', teacherSchema);
+module.exports = mongoose.model('College', collegeSchema);
